6a1c605d8fa8d03e7edcceddcd173a9cae0efa17
Nareshcodes <nareshcodes@gmail.com>
2025-07-18 15:21:21 +0545

    changed the api endpoint

--- src/service/http.js
+++ src/service/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL:"http://192.168.0.7:8125/api/",
+    baseURL:"https://pusoenotes.naresh-chaudhary.com.np/api/",
     headers:{
         "Content-Type":"application/json"
     }
